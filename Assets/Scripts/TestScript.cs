587dc1307a1dcfdfd415fc3b229ff330f15f3ed2
Amir Blum <amirblum@mbp.local>
2018-11-01 12:17:08 +0200

    Initial project commit!

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("The test script compiles and works!");
+    }
+}
