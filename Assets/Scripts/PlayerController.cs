132170f2f9acac5400935418c163b922f08d4240
kobi malul <kobi@cyberdomain.local>
2018-11-06 13:51:58 +0200

    Initial classes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerID;
+    private Vector2 position;
+
+    public void MoveCursor (Direction) {
+
+        if  (direction == up) {
+            Vector2 cursorPosition = Vector2(cursorPosition.x, cursorPosition.y + 1 )
+        }
+
+    }
+    
+    protected void Update()
+    {
+       var success = GameBoard.Instance.PlaceTile(playerID, 0, -1);
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileType
+    {
+        Empty = -1,
+        Organic = 0,
+        Mechanic = 1,
+    }
+
+    public TileType tileType;
+}
