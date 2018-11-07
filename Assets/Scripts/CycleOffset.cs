4a9eaab7a97650bf9e08194e70241b78b8720375
Amir Blum <amirblum@mbp.local>
2018-11-07 14:19:44 +0200

    Attempted to cycle animation offset

---
+++ Assets/Scripts/CycleOffset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleOffset : MonoBehaviour
+{
+    protected void OnEnable()
+    {
+        GetComponent<Animator>().SetFloat("Offset", Random.Range(0.0f, 1.0f));
+    }
+}
